49e4702b05f41e6554cfec6965e5053d7f3a2452
MatV <matvercau@gmail.com>
2023-02-15 17:49:20 +0100

    ajout de liste achats dans Client (ANTHONY.......... elle est ou ta boutique bordel, et l'heritage des consoles...)

--- NintendoProject/src/test/java/test/Test.java
+++ NintendoProject/src/test/java/test/Test.java
@@ -1,5 +1,10 @@
 package test;
-import model.*;
+import java.time.LocalDate;
+
+import model.Achat;
+import model.Client;
+import model.Console;
+import model.Jeu;
 
 public class Test {
 
@@ -8,13 +13,15 @@ public class Test {
 		Console c1 = new Console("Steam Deck", 350, "2021-08-22");
 		Console c2 = new Console("Nintendo Switch", 350, "2021-12-22");
 		
-		Jeu j1 = new Jeu("Dead Cells", c1);
-		Jeu j2 = new Jeu("Slay The Spire", c1);
-		Jeu j3 = new Jeu("Stardew Valley", c2);
-		Jeu j4 = new Jeu("Dicey Dungeons", c1);
-		Jeu j5 = new Jeu("Baba is you", c2);
+		Jeu j1 = new Jeu("Dead Cells", c1,b);
+		Jeu j2 = new Jeu("Slay The Spire", c1,b);
+		Jeu j3 = new Jeu("Stardew Valley", c2,b);
+		Jeu j4 = new Jeu("Dicey Dungeons", c1,b);
+		Jeu j5 = new Jeu("Baba is you", c2,b);
 		
+		Achat a = new Achat(j2,LocalDate.now(),0);
 		Client cl1 = new Client("Ney","Davout");
+		cl1.getAchats().add(a);
 		Client cl2 = new Client("Lannes","Murat");
 		
 		
