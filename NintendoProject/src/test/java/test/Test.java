c825789040539ec63e3a7646f0de610b2fc828e9
Marianne Guisset <marianneguisset@aol.com>
2023-02-15 17:05:27 +0100

    créations objets jeu & console

--- NintendoProject/src/test/java/test/Test.java
+++ NintendoProject/src/test/java/test/Test.java
@@ -1,9 +1,21 @@
 package test;
+import model.*;
 
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Console c1 = new Console("Steam Deck");
+		Console c2 = new Console("Nintendo Switch");
+		
+		Jeu j1 = new Jeu("Dead Cells", c1);
+		Jeu j2 = new Jeu("Slay The Spire", c1);
+		Jeu j3 = new Jeu("Stardew Valley", c2);
+		Jeu j4 = new Jeu("Dicey Dungeons", c1);
+		Jeu j5 = new Jeu("Baba is you", c2);
+		
+		
+		
 
 	}
 
